989e2d5cc65759dbf34ee31fe3499257fd27c637
NarrowGate <melvinwilson2012@gmail.com>
2019-01-05 22:53:56 +1000

    procfile and process env variable added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+var port = process.env.PORT || 8070;
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World! ksksksks');
+    res.write('Hello World! ksk2323sksks');
     res.end();
-  }).listen(8070);
+  }).listen(port);
 
